28a853513a0e07307f6aa9f61d2eeb232808b554
szaniasz <szewczyk.ann@gmail.com>
2023-05-08 14:18:52 +0200

    Bind click with board array and add changing the mark with every turn

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const boardGame = (function(){
     const board = [
+        ['','',''],
         ['X','','X'],
-        ['X','','X'],
-        ['','X','O']
+        ['','','O']
     ]
     const squares = document.querySelectorAll('.square')
     // const squaresArray = [...squares]
@@ -10,20 +10,44 @@ const boardGame = (function(){
         let squareNumber = 0
         for (let i = 0; i < board.length; i++) {
             for (let j = 0; j < this.board[i].length; j++) {
-              squares[squareNumber].innerHTML = this.board[i][j];
+              squares[squareNumber].innerText = this.board[i][j];
               squareNumber++
             }
         }
     }
-    
+    squares.forEach((square) => {
+        square.addEventListener("click", function() {
+        if (this.innerText === '') {
+            this.innerText = mark
+            const i = this.id[(this.id).length - 3]
+            const j = this.id[(this.id).length - 1]
+            boardGame.board[i][j] = mark
+            changeMark()
+        } else {
+            alert("You can't change the value")
+            return
+        }
+      })
+    })
+    // const checkIfFilled = function() {
+        
+    // }
+    let mark = 'X'
+    const changeMark = function() {
+        if (mark === 'X') {
+            mark = 'O'
+        } else {
+            mark = 'X'
+        }
+    }
     return {
         board, 
         displayBoard
     }
 })()
-// boardGame.squares.forEach((square) => {
-//     square.addEventListener('click', console.log('elo'))
-// })
+
+
+
 boardGame.displayBoard()
 
 
