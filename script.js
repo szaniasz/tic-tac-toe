7608cfd676eb1a5d4bbb4dfa4cf8c3bb4840cca5
szaniasz <szewczyk.ann@gmail.com>
2023-05-08 15:28:56 +0200

    Add logic for checking who won the game

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const boardGame = (function(){
     const board = [
         ['','',''],
-        ['X','','X'],
-        ['','','O']
+        ['','',''],
+        ['','','']
     ]
     const squares = document.querySelectorAll('.square')
     // const squaresArray = [...squares]
     const displayBoard = function() {   
         let squareNumber = 0
-        for (let i = 0; i < board.length; i++) {
+        for (let i = 0; i < (this.board).length; i++) {
             for (let j = 0; j < this.board[i].length; j++) {
               squares[squareNumber].innerText = this.board[i][j];
               squareNumber++
@@ -21,7 +21,9 @@ const boardGame = (function(){
             this.innerText = mark
             const i = this.id[(this.id).length - 3]
             const j = this.id[(this.id).length - 1]
-            boardGame.board[i][j] = mark
+            board[i][j] = mark
+            // console.log(board[i])
+            checkIfWon(i,j)
             changeMark()
         } else {
             alert("You can't change the value")
@@ -29,9 +31,7 @@ const boardGame = (function(){
         }
       })
     })
-    // const checkIfFilled = function() {
-        
-    // }
+
     let mark = 'X'
     const changeMark = function() {
         if (mark === 'X') {
@@ -40,9 +40,25 @@ const boardGame = (function(){
             mark = 'X'
         }
     }
+    const checkIfWon = function(i, j) {
+        if (board[i].every (el => el === board[i][0])) {
+            console.log('the same in row. the winner is ' + mark)
+
+        } else if (board[0][j] === board[1][j] && board[1][j] === board[2][j]) {
+            console.log('the winner is ' + mark)
+        } else if ( (j % 2 === 0 && i % 2 === 0) || (i === j)) {
+            if ( (board[0][0] === board[1][1] && board[1][1] === board[2][2]) ||
+            (board[0][2] === board[1][1] && board[1][1] === board[2][0]) ){
+                console.log('the winner is ' + mark)
+            }
+        } else {
+            return
+        }
+    }
     return {
-        board, 
-        displayBoard
+        board,
+        displayBoard,
+        checkIfWon
     }
 })()
 
