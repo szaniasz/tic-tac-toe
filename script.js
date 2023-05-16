1afac01a27dce0e5c0130cd4c583c99f4dbcec72
szaniasz <szewczyk.ann@gmail.com>
2023-05-16 13:33:24 +0200

    Add restart button and change css

--- script.js
+++ script.js
@@ -36,6 +36,8 @@ const boardGame = (function(){
 
     const player1Name = document.getElementById('player1')
     const player2Name = document.getElementById('player2')
+    let player1Result = document.getElementById('player1result')
+    let player2Result = document.getElementById('player2result')
 
     const changeMark = function() {
         if (mark === 'X') {
@@ -59,23 +61,33 @@ const boardGame = (function(){
         } else if (board[0][j] === board[1][j] && board[1][j] === board[2][j]) {
             displayWinner()
             showModal()
-
         } else if ( (board[0][0] === board[1][1] && board[1][1] === board[2][2] && board[1][1] === mark) ||
         (board[0][2] === board[1][1] && board[1][1] === board[2][0] && board[1][1] === mark)) {
             displayWinner()
             showModal()
-
         } else {
             return
         }
     }
+
+    const changeResultValue = function() {
+        if(player1Name.innerText === winner) {
+            player1Result.innerText = parseInt(player1Result.innerText) +1
+        } else if (player2Name.innerText === winner) {
+            player2Result.innerText = parseInt(player2Result.innerText) +1
+        }
+    }
+
     const nameOfWinner = document.querySelector('.modal-winner')
-    const modal = document.querySelector('.modal')
-    const overlay = document.getElementById('overlay')
     const displayWinner = function() {
         winner = player
         nameOfWinner.innerText = winner
     }
+  
+    const modal = document.querySelector('.modal')
+    const overlay = document.getElementById('overlay')
+    
+
     const showModal = function(){
         modal.classList.add('active')
         overlay.classList.add('active')
@@ -83,6 +95,7 @@ const boardGame = (function(){
     const hideModal = function(){
         modal.classList.remove('active')
         overlay.classList.remove('active')
+        changeResultValue() 
     }
     const modalButton = document.querySelector('[data-modal-button]')
     modalButton.addEventListener('click', function() {
@@ -98,6 +111,18 @@ const boardGame = (function(){
             }
         }
     }
+
+    const restartButton = document.getElementById('restart')
+
+    restartButton.addEventListener(('click'), function(){
+        if (window.confirm("Are your sure? It can't be undone")) {
+            restartGame()
+            displayBoard()
+            player1Result.innerText = 0
+            player2Result.innerText = 0
+          } 
+    })
+
     return {
         board,
         displayBoard,
